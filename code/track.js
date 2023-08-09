8f17c9b5f5cd4ce62d202c9fc3dd94d12bbf1047
bnoob7 <binodgurung690@gmail.com>
2023-08-09 18:26:08 +0545

    initialize project

---
+++ code/track.js
@@ -0,0 +1,8 @@
+// better to use polygon to get the targeted location if.e destination location. 
+// better to use polyline to show the distance betweeen the source and the destination Point/
+
+// better to use the marker than layer
+// key difference between circle marker and the layer is 
+// marker is generally done in px that means its dynamic with respect to the Map. 
+// however the layer length is given in meter which is fixed and static in nature. 
+// use case : generally to show ther geo location layering is done, and to put the location of the kandmark the marker is used
